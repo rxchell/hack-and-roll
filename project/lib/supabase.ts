43bde390798b9eff7f0e1748e10c027b7e37667e
Monochromas <73582380+Monochromas@users.noreply.github.com>
2025-01-05 21:51:28 +0800

    basic stuff. Template to update and sign up. Needs to connect users with auth.id

---
+++ project/lib/supabase.ts
@@ -0,0 +1,14 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = "https://jcccjsgpxupodjnpbrko.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpjY2Nqc2dweHVwb2RqbnBicmtvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4OTI2MDYsImV4cCI6MjA1MTQ2ODYwNn0.YEwK-UAJrkVhNRNl4nC0ACyo-ST2eFVVuCCprji6nL4"
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
