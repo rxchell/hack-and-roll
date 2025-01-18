cedb08932b41ec9e4436b822074c68bdc13c831f
Rachel Tai Ke Jia <e0969575@u.nus.edu>
2025-01-18 11:57:22 +0800

    Add logo and styling

--- project/components/Voucher/Voucher.tsx
+++ project/components/Voucher/Voucher.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { supabase } from '../../lib/supabase'
 import { StyleSheet } from 'react-native'
 import { View, Text, Image, TouchableOpacity, FlatList, ActivityIndicator, } from 'react-native';
