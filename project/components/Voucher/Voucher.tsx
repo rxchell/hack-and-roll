5d8ec7ea255a6757714e53e96fd65ae60154e278
Rachel Tai Ke Jia <e0969575@u.nus.edu>
2025-01-18 11:32:43 +0800

    Styling of vouchers

--- project/components/Voucher/Voucher.tsx
+++ project/components/Voucher/Voucher.tsx
@@ -86,9 +86,9 @@ export default function Voucher({ session }: { session: Session }) {
                     <View style={styles.row}>
                         <Text style={styles.name}>{item.name}</Text>
                     </View>
-                    <Text>Description: {item.description}</Text>
+                    <Text style={styles.description}>Description: {item.description}</Text>
                     <RedeemButton/>
-                    <Text>Cost: {item.cost}</Text>
+                    <Text style={styles.cost}>Cost: {item.cost}</Text>
                 </View>
                 <View style={styles.columnRight}>
                     <Ionicons name={icon} size={90}></Ionicons>
